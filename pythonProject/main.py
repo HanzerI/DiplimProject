107d6d9be50cb2b7f0b245eaf473126aa6735d0e
HanzerI <forvpinj@gmail.com>
2023-08-16 21:12:16 +0400

    Version 1.2. The Python project has been changed

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,21 +1,35 @@
+from flask import Flask, request, jsonify
 from dostoevsky.tokenization import RegexTokenizer
 from dostoevsky.models import FastTextSocialNetworkModel
 
+app = Flask(__name__)
+
 tokenizer = RegexTokenizer()
-tokens = tokenizer.split('всё очень плохо')  # [('всё', None), ('очень', None), ('плохо', None)]
+model = FastTextSocialNetworkModel(tokenizer=tokenizer)
 
-model = FastTextSocialNetworkModel(tokenizer)
 
-messages = [
-    'привет',
-    'я люблю тебя!!',
-    'малолетние дебилы'
-]
+def analyze_text(text):
+    result = model.predict([text], k=2)
+    return result[0]
 
-results = model.predict(messages, k=2)
 
-for message, sentiment in zip(messages, results):
-    # привет -> {'speech': 1.0000100135803223, 'skip': 0.0020607432816177607}
-    # люблю тебя!! -> {'positive': 0.9886782765388489, 'skip': 0.005394937004894018}
-    # малолетние дебилы -> {'negative': 0.9525841474533081, 'neutral': 0.13661839067935944}]
-    print(message, '->', sentiment)
+@app.route('/analyze', methods=['POST'])
+def analyze_endpoint():
+    try:
+        data = request.get_json()
+        texts = data.get('texts', [])
+
+        if not texts:
+            return jsonify({'error': 'No texts provided'}), 400
+
+        results = {}
+        for text in texts:
+            results[text] = analyze_text(text)
+
+        return jsonify({'results': results}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
